1bf69df0e1fa39a3f11eef08052c0e23ebe68a3d
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-30 14:21:03 +0100

    Threading - Basics

---
+++ Threading_Demo/Threading_Demo/Demo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading_Demo
+{
+    class Demo
+    {
+        private int wert;
+        private object syncObject = new object();
+        public void MachWas()
+        {
+            while(true)
+            {
+                lock (syncObject)
+                {
+                    wert++;
+                    if (wert > 100)
+                        break;
+                    Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: {wert}");
+                }
+
+                // Alternative:
+                // Interlocked.Increment(ref wert);
+            }
+        }
+    }
+}
--- Threading_Demo/Threading_Demo/Program.cs
+++ Threading_Demo/Threading_Demo/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Threading_Demo
@@ -10,6 +11,58 @@ namespace Threading_Demo
     {
         static void Main(string[] args)
         {
+            #region Thread - Allgemein
+            //Thread t1 = new Thread(Punkterl);
+            //Thread t2 = new Thread(Stricherl);
+
+            //t1.Start();
+            //t2.Start();
+
+            // Warten
+            //t1.Join();
+            //t2.Join();
+
+            //Thread.Sleep(3000);
+            //t1.Abort();
+            //t2.Abort();
+            //t1.Join();
+            //t2.Join(); 
+            #endregion
+
+            Demo d1 = new Demo();
+            Thread t1 = new Thread(d1.MachWas);
+            Thread t2 = new Thread(d1.MachWas);
+
+            t1.Start();
+            t2.Start();
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+
+        private static void Stricherl()
+        {
+            try
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Thread.Sleep(100);
+                    Console.Write('-');
+                }
+            }
+            catch (ThreadAbortException)
+            {
+                Console.WriteLine("Thread 'Stricherl' wurde abgebrochen");
+            }
+        }
+
+        private static void Punkterl()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(500);
+                Console.Write('.');
+            }
         }
     }
 }
