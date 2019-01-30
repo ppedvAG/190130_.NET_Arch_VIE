65d41ff4f7d35830587871e7bfa2e1c97aaf6b2b
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-30 11:38:21 +0100

    Builder_Demo

--- Builder_Demo/Builder_Demo/Program.cs
+++ Builder_Demo/Builder_Demo/Program.cs
@@ -10,6 +10,21 @@ namespace Builder_Demo
     {
         static void Main(string[] args)
         {
+            var ikeaSchrank = Schrank.BaueSchrank()
+                                     .MitTüren(2)
+                                     .MitBöden(2)
+                                     .HatKleiderstange(true)
+                                     .KonstruiereSchrank();
+
+            var xxxLutzSchrank = Schrank.BaueSchrank()
+                                        .MitTüren(4)
+                                        .MitBöden(1)
+                                        .InFarbe("Grün")
+                                        .MitOberfläche(Oberflächenart.Lackiert)
+                                        .KonstruiereSchrank();
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
---
+++ Builder_Demo/Builder_Demo/Schrank.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder_Demo
+{
+    public enum Oberflächenart { Unbehandelt,Gewachst,Lackiert};
+    class Schrank
+    {
+        private Schrank(){}
+
+        public int AnzahlTüren { get; set; }
+        public Oberflächenart Oberfläche { get; set; }
+        public string Farbe { get; set; }
+        public bool Metallschienen { get; set; }
+        public int AnzahlBöden { get; set; }
+        public bool Kleiderstange { get; set; }
+
+        public static SchrankBauer BaueSchrank() => new SchrankBauer();
+        public class SchrankBauer
+        {
+            public SchrankBauer()
+            {
+                schrank = new Schrank();
+            }
+            private Schrank schrank;
+            public SchrankBauer MitTüren(int anzahl)
+            {
+                schrank.AnzahlTüren = anzahl;
+                return this;
+            }
+            public SchrankBauer MitOberfläche(Oberflächenart oberflächenart)
+            {
+                schrank.Oberfläche = oberflächenart;
+                return this;
+            }
+            public SchrankBauer InFarbe(string farbe)
+            {
+                schrank.Farbe = farbe;
+                return this;
+            }
+            public SchrankBauer HatMetallSchienen(bool schienenVorhanden)
+            {
+                schrank.Metallschienen = schienenVorhanden;
+                return this;
+            }
+            public SchrankBauer MitBöden(int anzahlBöden)
+            {
+                schrank.AnzahlBöden = anzahlBöden;
+                return this;
+            }
+            public SchrankBauer HatKleiderstange(bool kleiderstangeVorhanden)
+            {
+                schrank.Kleiderstange = kleiderstangeVorhanden;
+                return this;
+            }
+
+            public Schrank KonstruiereSchrank()
+            {
+                if (schrank.AnzahlTüren < 2 || schrank.AnzahlTüren > 7)
+                    throw new ArgumentException("Der Schrank darf nur zwischen 2 und 7 Türen haben");
+
+                if (!string.IsNullOrEmpty(schrank.Farbe) && schrank.Oberfläche != Oberflächenart.Lackiert)
+                    throw new ArgumentException("Nur ein lackierter Schrank darf eine Farbe haben");
+
+                if (schrank.AnzahlBöden < 0 || schrank.AnzahlBöden > 6)
+                    throw new ArgumentException("Der Schrank darf nur zwischen 0 und 6 Böden haben");
+
+                return schrank;
+            }
+        }
+    }
+}
