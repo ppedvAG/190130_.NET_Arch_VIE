f9986279275e96530695bf04ca12fe36be0e7649
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-31 12:04:32 +0100

    Mini-UI mit Testdaten

--- MovieDatabase/ppedv.MovieDatabase.UI.Konsole/Program.cs
+++ MovieDatabase/ppedv.MovieDatabase.UI.Konsole/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ppedv.MovieDatabase.Data.EF;
+using ppedv.MovieDatabase.Domain;
+using ppedv.MovieDatabase.Logic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,19 @@ namespace ppedv.MovieDatabase.UI.Konsole
     {
         static void Main(string[] args)
         {
+            Core core = new Core(new EFRepository(new EFContext(@"Server=(localDB)\MSSQLLocalDB;Database=MovieDatabase;Trusted_Connection=true;AttachDbFilename=C:\temp\db.mdf")));
+
+            if (core.Repository.Query<Person>().Count() == 0)
+                core.CreateDemoData();
+
+            foreach (Person person in core.Repository.GetAll<Person>())
+            {
+                Console.WriteLine($"{person.FirstName} {person.LastName}- Age: {person.Age}");
+            }
+
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
