b8d397ed78c2236a277a6f078284556d6fb6f5ce
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-31 15:18:07 +0100

    StreamProvider erstellt

--- MovieDatabase/ppedv.MovieDatabase.Domain/StreamProvider.cs
+++ MovieDatabase/ppedv.MovieDatabase.Domain/StreamProvider.cs
@@ -7,7 +7,7 @@ namespace ppedv.MovieDatabase.Domain
     public class StreamProvider : Entity
     {
         public string Name { get; set; }
-        public int[] Movies { get; set; }
+        public string Movies { get; set; } // "12,2,3,4,5," ....
         public decimal MonthlySubscriptionRate { get; set; }
     }
 }
