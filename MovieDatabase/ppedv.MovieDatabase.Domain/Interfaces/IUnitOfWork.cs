292ff269a966359f1fe52b31d3f54562fd69f20e
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-31 15:18:47 +0100

    Konsole mit mehreren UoW

--- MovieDatabase/ppedv.MovieDatabase.Domain/Interfaces/IUnitOfWork.cs
+++ MovieDatabase/ppedv.MovieDatabase.Domain/Interfaces/IUnitOfWork.cs
@@ -1,9 +1,12 @@
-﻿namespace ppedv.MovieDatabase.Domain.Interfaces
+﻿using System;
+
+namespace ppedv.MovieDatabase.Domain.Interfaces
 {
     public interface IUnitOfWork
     {
         // Spezialvariante fest einbauen
         IPersonRepository PersonRepository { get; }
+        Type[] SupportedTypes { get; }
 
         // Generelle-Variante: Repository generieren
         IUniversalRepository<T> GetRepository<T>() where T : Entity, new();
--- MovieDatabase/ppedv.MovieDatabase.UI.Konsole/Program.cs
+++ MovieDatabase/ppedv.MovieDatabase.UI.Konsole/Program.cs
@@ -1,6 +1,8 @@
 ﻿using ppedv.MovieDatabase.Data.EF;
+using ppedv.MovieDatabase.Data.SQLite;
 using ppedv.MovieDatabase.Domain;
 using ppedv.MovieDatabase.Logic;
+using SQLite;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,17 +15,34 @@ namespace ppedv.MovieDatabase.UI.Konsole
     {
         static void Main(string[] args)
         {
-            Core core = new Core(new EFUnitOfWork(new EFContext(@"Server=(localDB)\MSSQLLocalDB;Database=MovieDatabase;Trusted_Connection=true;AttachDbFilename=C:\temp\db.mdf")));
+            Core core = new Core(new EFUnitOfWork(new EFContext(@"Server=(localDB)\MSSQLLocalDB;Database=MovieDatabase;Trusted_Connection=true;AttachDbFilename=C:\temp\db.mdf")),
+                                 new SQLiteUnitOfWork(new SQLiteConnection("db.sqlite")));
 
-            if (core.UnitOfWork.PersonRepository.Query().Count() == 0) // Spezifisches Repo
+            if (core.GetUnitOfWorkForType<Person>().PersonRepository.Query().Count() == 0) // Spezifisches Repo
                 core.CreateDemoData();
 
-            foreach (Person person in core.UnitOfWork.GetRepository<Person>().GetAll()) // Generisches Repo
+            foreach (Person person in core.GetUnitOfWorkForType<Person>().GetRepository<Person>().GetAll()) // Generisches Repo
             {
                 Console.WriteLine($"{person.FirstName} {person.LastName}- Age: {person.Age}");
             }
 
-            Console.WriteLine(core.UnitOfWork.PersonRepository.GetPersonWithHighestSalary().LastName);
+            Console.WriteLine(core.GetUnitOfWorkForType<Person>().PersonRepository.GetPersonWithHighestSalary().LastName);
+            Console.WriteLine("--------------------------");
+
+            var movieRepository = core.GetUnitOfWorkForType<Movie>().GetRepository<Movie>();
+            foreach (StreamProvider p in core.GetUnitOfWorkForType<StreamProvider>().GetRepository<StreamProvider>().GetAll())
+            {
+                Console.WriteLine(p.Name);
+                Movie[] movies = p.Movies.Split(',').Select(x => movieRepository.GetByID(Convert.ToInt32(x)))
+                                                    .ToArray();
+                foreach (Movie m in movies)
+                {
+                    if (m == null) continue;
+                    Console.WriteLine(m.Title);
+                    Console.WriteLine(m.ReleaseDate);
+                }
+                Console.WriteLine("---");
+            }
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
