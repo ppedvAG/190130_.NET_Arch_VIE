6b1e980210ae36602e44c39969cb25e4aecdb6ec
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-31 11:00:20 +0100

    Query hinzugefügt

--- MovieDatabase/ppedv.MovieDatabase.Domain/Interfaces/IRepository.cs
+++ MovieDatabase/ppedv.MovieDatabase.Domain/Interfaces/IRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ppedv.MovieDatabase.Domain.Interfaces
@@ -7,6 +8,7 @@ namespace ppedv.MovieDatabase.Domain.Interfaces
     public interface IRepository
     {
         IEnumerable<T> GetAll<T>() where T : Entity;
+        IQueryable<T> Query<T>() where T : Entity;
         T GetByID<T>(int ID) where T : Entity;
         void Add<T>(T item) where T : Entity;
         void Delete<T>(T item) where T : Entity;
