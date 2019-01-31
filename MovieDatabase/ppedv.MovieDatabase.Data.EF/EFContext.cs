ce9d35a6a4541097c5fba5e319f2292cb03176a1
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-31 09:41:50 +0100

    EF - Minimalconfig

---
+++ MovieDatabase/ppedv.MovieDatabase.Data.EF/EFContext.cs
@@ -0,0 +1,18 @@
+﻿using ppedv.MovieDatabase.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace ppedv.MovieDatabase.Data.EF
+{
+    // Minimale Basisconfig
+    public class EFContext : DbContext
+    {
+        public EFContext(string connectionString) : base(connectionString){}
+
+        public DbSet<Movie> Movie { get; set; }
+        public DbSet<Person> Person { get; set; }
+        public DbSet<MovieTheater> MovieTheater { get; set; }
+    }
+}
