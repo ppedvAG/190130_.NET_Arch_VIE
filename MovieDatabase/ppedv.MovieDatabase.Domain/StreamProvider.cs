2a8769c5afbcb8ed5fb64cb6adedf14c8c4924d3
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-31 14:19:38 +0100

    StreamProvider für zweite DB-Layer erstellt

---
+++ MovieDatabase/ppedv.MovieDatabase.Domain/StreamProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ppedv.MovieDatabase.Domain
+{
+    public class StreamProvider : Entity
+    {
+        public string Name { get; set; }
+        public int[] Movies { get; set; }
+        public decimal MonthlySubscriptionRate { get; set; }
+    }
+}
