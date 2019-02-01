62f4e57f2813ef440dd66c2027d4d71e84d60cbe
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-02-01 09:21:47 +0100

    Reflection_Demo

--- Reflection_Demo/Reflection_Demo/Program.cs
+++ Reflection_Demo/Reflection_Demo/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,30 @@ namespace Reflection_Demo
     {
         static void Main(string[] args)
         {
+            double i = 42;
+            Type t = i.GetType();
 
+            Console.WriteLine(t);
+
+            // Instanz erstellen
+            var neueZahl = Activator.CreateInstance(t);
+            Console.WriteLine(neueZahl);
+            Console.WriteLine(neueZahl.GetType());
+
+            // Laden einer Assembly
+            var assembly = Assembly.LoadFrom("MeineLib.dll");
+            Type taschenrechnerTyp = assembly.GetType("MeineLib.Taschenrechner");
+            var tr = Activator.CreateInstance(taschenrechnerTyp);
+            Console.WriteLine(tr);
+
+            // Aufrufen von Methoden:
+            var methode = taschenrechnerTyp.GetRuntimeMethod("Add", new Type[] { typeof(int), typeof(int) });
+            var erg = methode.Invoke(tr, new object[] { 12, 5 });
+
+            Console.WriteLine(erg);
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
