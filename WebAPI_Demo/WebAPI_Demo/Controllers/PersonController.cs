c192d2d5b4f6397d536e6d209ef5ffb572de9bf0
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-02-01 12:05:27 +0100

    Mini-WebAPI erstellt

---
+++ WebAPI_Demo/WebAPI_Demo/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI_Demo.Models;
+
+namespace WebAPI_Demo.Controllers
+{
+    public class PersonController : ApiController
+    {
+        // IRL: aus EF laden
+        private Person[] personen = new Person[]
+        {
+            new Person{ID=0,Vorname="Tom",Nachname="Ate",Alter=10,Gehalt=100},
+            new Person{ID=1,Vorname="Anna",Nachname="Nass",Alter=20,Gehalt=200},
+            new Person{ID=2,Vorname="Peter",Nachname="Silie",Alter=30,Gehalt=300}
+        };
+
+        public IEnumerable<Person> GetPeople() => personen;
+
+        public IHttpActionResult GetPerson(int id)
+        {
+            var p = personen.FirstOrDefault(x => x.ID == id);
+            if (p != null)
+                return Ok(p);
+            else
+                return NotFound();
+        }
+
+    }
+}
---
+++ WebAPI_Demo/WebAPI_Demo/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI_Demo.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public byte Alter { get; set; }
+        public decimal Gehalt { get; set; }
+    }
+}
